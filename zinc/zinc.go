25ec00c2bc2330ac128ba0f2d0d550d685937249
Jose Mata <mata649@hotmail.comm>
2023-01-05 19:08:03 -0600

    added ZinResponse, function now returns a ZincResp

--- zinc/zinc.go
+++ zinc/zinc.go
@@ -2,7 +2,7 @@ package zinc
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"log"
@@ -12,7 +12,16 @@ import (
 	"github.com/mata649/mail_indexer/config"
 )
 
-func MakeRequest(filePath string, wg *sync.WaitGroup, semaphore chan bool, currentConfig config.Configuration) {
+type ZincResponse struct {
+	Message     string `json:"message"`
+	RecordCount int    `json:"record_count"`
+}
+
+// Makes a request to the given URL with the provided configuration.
+// The request is a POST request with the JSON payload read from the specified file path.
+// If the request is successful, the function prints the status code and the response body.
+// Otherwise, it prints an error message.
+func MakeRequest(filePath string, wg *sync.WaitGroup, semaphore chan bool, currentConfig config.Configuration) ZincResponse {
 	semaphore <- true
 	jsonStr, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -31,12 +40,20 @@ func MakeRequest(filePath string, wg *sync.WaitGroup, semaphore chan bool, curre
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
+
 	log.Println(resp.StatusCode)
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(body))
+	log.Println(string(body))
+
+	zincResponse := ZincResponse{}
+	err = json.Unmarshal(body, &zincResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
 	wg.Done()
 	<-semaphore
+	return zincResponse
 }
