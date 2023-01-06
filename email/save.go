346fcadbbcdc2cdc5bad71face096ceaf738bcc2
Jose Mata <mata649@hotmail.comm>
2023-01-05 19:09:15 -0600

    now file is opened once and passed by reference

--- email/save.go
+++ email/save.go
@@ -4,36 +4,42 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
 
-func addLine(currentFilePath string, line string) {
+// Appends a line to a file.
+// It takes the line to append and the file as arguments.
+func addLine(line string, f *os.File) {
 
-	f, err := os.OpenFile(currentFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-	_, err = io.WriteString(f, line+"\n")
+	_, err := io.WriteString(f, line+"\n")
 	if err != nil {
 		fmt.Println(err)
 	}
 }
+
+// Converts a slice of Email structs to a slice of JSON strings and
+// appends them to a file. The file is created if it does not exist.
 func saveEmails(emails []Email, counter int, currentDir string) {
 
 	currentFilePath := filepath.Join(currentDir, fmt.Sprintf("file%v.ndjson", counter))
 	_, err := os.Create(currentFilePath)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fmt.Printf("Saving: %v \n", currentFilePath)
+	f, err := os.OpenFile(currentFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
 
 	for _, email := range emails {
 		parsedEmail, _ := json.Marshal(email)
-		addLine(currentFilePath, `{ "index" : { "_index" : "emails" } }`)
-		addLine(currentFilePath, string(parsedEmail))
+		addLine(`{ "index" : { "_index" : "emails" } }`, f)
+		addLine(string(parsedEmail), f)
 
 	}
-	fmt.Printf("Saved: %v \n", currentFilePath)
+	log.Printf("Saved: %v \n", currentFilePath)
 }
